2b7f73339cd21ad5a1bc58d27a392c1f533f9616
sumithanvat <skhanvat@gmail.com>
2023-03-23 00:47:10 +0530

    Added:Given 3 Strings find the maximum

--- com/bridgelabz/Main.java
+++ com/bridgelabz/Main.java
@@ -3,14 +3,14 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args) {
         // Test cases
-        FindMax<Float> maxfloat = new FindMax<Float>();
-        float max1 = maxfloat.getMax(3.2f,2.8f,4.6f);
-        float max2 = maxfloat.getMax(1.3f, 3.5f, 2.6f);
-        float max3 = maxfloat.getMax(1.6f, 3.7f, 4.6f);
+        FindMax<String> maxString = new FindMax<String>();
+        String max1 = maxString.getMax("r","s","g");
+        String max2 = maxString.getMax("A","p","z");
 
-        System.out.println("Max of 10, 5, 75 is: " + max1);
-        System.out.println("Max of 10, 30, 20 is: " + max2);
-        System.out.println("Max of 155, 375, 200 is: " + max3);
+
+        System.out.println("Max of r , s, g is: " + max1);
+        System.out.println("Max of A, p, z is: " + max2);
+
 
 
     }
