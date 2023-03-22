1dd9cc0f70ab12ad4222d8fd948c33b076a428c1
sumithanvat <skhanvat@gmail.com>
2023-03-23 00:32:58 +0530

    Added:Given 3 Floats find the maximum

--- com/bridgelabz/Main.java
+++ com/bridgelabz/Main.java
@@ -3,10 +3,10 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args) {
         // Test cases
-        FindMax<Integer> maxInteger = new FindMax<Integer>();
-        Integer max1 = maxInteger.getMax(10, 5, 75);
-        Integer max2 = maxInteger.getMax(10, 30, 20);
-        Integer max3 = maxInteger.getMax(155, 375, 200);
+        FindMax<Float> maxfloat = new FindMax<Float>();
+        float max1 = maxfloat.getMax(3.2f,2.8f,4.6f);
+        float max2 = maxfloat.getMax(1.3f, 3.5f, 2.6f);
+        float max3 = maxfloat.getMax(1.6f, 3.7f, 4.6f);
 
         System.out.println("Max of 10, 5, 75 is: " + max1);
         System.out.println("Max of 10, 30, 20 is: " + max2);
